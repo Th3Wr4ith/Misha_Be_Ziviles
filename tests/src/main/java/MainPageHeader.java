7189ae95c30985a3a88e28f4aedbd49afe8d2207
Inga <Inga.Stankuviene@stud.vtmc.lt>
2023-04-08 01:28:22 +0300

    Jau veikia bandomieji testai

--- tests/src/main/java/MainPageHeader.java
+++ tests/src/main/java/MainPageHeader.java
@@ -3,11 +3,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MainPageHeader extends PageObject{
-    @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/h6[1]/img")
+    @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/p/img")
     private WebElement logo;
 
     @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/h6")
     private WebElement greeting;
+
 
     public MainPageHeader(WebDriver driver) {super(driver); }
 
@@ -16,5 +17,6 @@ public class MainPageHeader extends PageObject{
     public void visibleGreeting() {
         greeting.isDisplayed();
     }
+
 }
 
--- tests/src/test/java/HeaderTest.java
+++ tests/src/test/java/HeaderTest.java
@@ -1,4 +1,8 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HeaderTest extends BaseTest {
     @Test
