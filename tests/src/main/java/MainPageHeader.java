b4ebe6e12a50410de0c1284edd756832947fd4ec
Inga <Inga.Stankuviene@stud.vtmc.lt>
2023-04-08 02:12:12 +0300

    Search button

--- tests/src/main/java/MainPageHeader.java
+++ tests/src/main/java/MainPageHeader.java
@@ -2,19 +2,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class MainPageHeader extends PageObject{
-    @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/p/img")
+public class MainPageHeader extends PageObject {
+    @FindBy(xpath = "//*[@id=\"root\"]/div/header/div/div/p/img")
     private WebElement logo;
 
-    @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/h6")
+    @FindBy(xpath = "//*[@id=\"root\"]/div/header/div/div/h6")
     private WebElement greeting;
 
-    public MainPageHeader(WebDriver driver) {super(driver); }
+    @FindBy(xpath = "//*[@id=\"root\"]/div/header/div/div/div/button[1]")
+    private WebElement searchButton;
 
-    public void visibleLogo() { logo.isDisplayed();}
+    public MainPageHeader(WebDriver driver) {
+        super(driver);
+    }
+
+    public void visibleLogo() {
+        logo.isDisplayed();
+    }
 
     public void visibleGreeting() {
         greeting.isDisplayed();
+    }
+
+    public WebElement setSearchButton() {
+        if (searchButton.isDisplayed()) {
+            searchButton.click();
+        }
+        return searchButton;
     }
 }
 
