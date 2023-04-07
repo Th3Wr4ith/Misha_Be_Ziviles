302859984f3aac9c012ca72b9be373d88ed2bec0
Inga <Inga.Stankuviene@stud.vtmc.lt>
2023-04-08 00:59:55 +0300

    Testai veikia

--- tests/src/main/java/MainPageHeader.java
+++ tests/src/main/java/MainPageHeader.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MainPageHeader extends PageObject{
-    @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/h6[1]/img")
+    @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/p/img")
     private WebElement logo;
 
     @FindBy (xpath = "//*[@id=\"root\"]/div/header/div/div/h6")
